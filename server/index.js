5c0408a987932df73d183bcef11f80f0a62c5233
artbohr <bohrcs@gmail.com>
2018-02-02 22:32:31 +0100

    Add more basic configs

--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
-
+const mongo = require("mongodb");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+mongoose.connect(process.env.MONGO_URI);
 // Priority serve any static files.
 app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
+// parse application/json
+app.use(bodyParser.json());
 
 // Answer API requests.
 app.get('/api', function (req, res) {
