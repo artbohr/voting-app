21d8ab94825f7cde507c9dfb6caf3a799594e700
artbohr <bohrcs@gmail.com>
2018-02-21 04:56:31 +0100

    Add chartjs setup

--- react-ui/src/components/ShowPoll.js
+++ react-ui/src/components/ShowPoll.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import {Doughnut} from 'react-chartjs-2';
 
 class ShowForm extends React.Component {
   render() {
+
     return (
       <div className="panel panel-default">
         <div className="panel-heading">
@@ -9,13 +11,19 @@ class ShowForm extends React.Component {
         </div>
         <div className="panel-body">
           <form>
-            <div className="form-group">
+            <div className="row form-group">
+              <div className="col-sm-5">
               <label>Select Option</label>
               <select className="form-control">
                   {this.props.pollOptions}
               </select>
+              <br/>
+              <button className='btn btn-block btn-primary center-block' type='submit'>Vote</button>
+              </div>
+              <div className="col-sm-5">
+              <Doughnut data={[1,2]} width={100} height={50} options={{maintainAspectRatio: false}}/>
+                </div>
             </div>
-            <button className='btn btn-block btn-primary center-block' type='submit'>Vote</button>
           </form>
         </div>
       </div>
